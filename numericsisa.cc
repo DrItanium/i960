b3e261fd3bb2789f14d99b0be493d27e73f4cd0e
Joshua Scoggins <Theoretically.x64@gmail.com>
2018-10-20 16:15:18 -0700

    Fix DRY violations with the classify routines

--- numericsisa.cc
+++ numericsisa.cc
@@ -106,83 +106,37 @@ namespace i960 {
         auto sval = src.get<ByteOrdinal>();
         _ac.conditionCode = ((sval >= 0b0011000) && (sval <= 0b00111001)) ? 0b000 : 0b010;
     }
-    void Core::classr(Core::SourceRegister src) noexcept {
-		auto val = src.get<Real>();
+	template<typename T, typename I>
+	void performClassification(const T& src, ArithmeticControls& ac) noexcept {
+		I val = src.template get<I>();
 		auto s = (val.sign << 3) & 0b1000;
 		if (val.isZero()) {
-			_ac.arithmeticStatusField = s | 0b000;
+			ac.arithmeticStatusField = s | 0b000;
 		} else if (val.isDenormal()) {
-			_ac.arithmeticStatusField = s | 0b001;
+			ac.arithmeticStatusField = s | 0b001;
 		} else if (val.isNormal()) {
-			_ac.arithmeticStatusField = s | 0b010;
+			ac.arithmeticStatusField = s | 0b010;
 		} else if (val.isInfinity()) {
-			_ac.arithmeticStatusField = s | 0b011;
+			ac.arithmeticStatusField = s | 0b011;
 		} else if (val.isQuietNaN()) {
-			_ac.arithmeticStatusField = s | 0b100;
+			ac.arithmeticStatusField = s | 0b100;
 		} else if (val.isSignalingNaN()) {
-			_ac.arithmeticStatusField = s | 0b101;
+			ac.arithmeticStatusField = s | 0b101;
 		} else if (val.isReservedEncoding()) {
-			_ac.arithmeticStatusField = s | 0b110;
+			ac.arithmeticStatusField = s | 0b110;
 		}
+	}
+    void Core::classr(Core::SourceRegister src) noexcept {
+		performClassification<decltype(src), Real>(src, _ac);
     }
     void Core::classr(Core::ExtendedSourceRegister src) noexcept {
-		auto val = src.get<ExtendedReal>();
-		auto s = (val.sign << 3) & 0b1000;
-		if (val.isZero()) {
-			_ac.arithmeticStatusField = s | 0b000;
-		} else if (val.isDenormal()) {
-			_ac.arithmeticStatusField = s | 0b001;
-		} else if (val.isNormal()) {
-			_ac.arithmeticStatusField = s | 0b010;
-		} else if (val.isInfinity()) {
-			_ac.arithmeticStatusField = s | 0b011;
-		} else if (val.isQuietNaN()) {
-			_ac.arithmeticStatusField = s | 0b100;
-		} else if (val.isSignalingNaN()) {
-			_ac.arithmeticStatusField = s | 0b101;
-		} else if (val.isReservedEncoding()) {
-			_ac.arithmeticStatusField = s | 0b110;
-		}
+		performClassification<decltype(src), ExtendedReal>(src, _ac);
     }
     void Core::classrl(Core::LongSourceRegister src) noexcept {
-        //TODO implement
-		auto val = src.get<LongReal>();
-		auto s = (val.sign << 3) & 0b1000;
-		if (val.isZero()) {
-			_ac.arithmeticStatusField = s | 0b000;
-		} else if (val.isDenormal()) {
-			_ac.arithmeticStatusField = s | 0b001;
-		} else if (val.isNormal()) {
-			_ac.arithmeticStatusField = s | 0b010;
-		} else if (val.isInfinity()) {
-			_ac.arithmeticStatusField = s | 0b011;
-		} else if (val.isQuietNaN()) {
-			_ac.arithmeticStatusField = s | 0b100;
-		} else if (val.isSignalingNaN()) {
-			_ac.arithmeticStatusField = s | 0b101;
-		} else if (val.isReservedEncoding()) {
-			_ac.arithmeticStatusField = s | 0b110;
-		}
+		performClassification<decltype(src), LongReal>(src, _ac);
     }
     void Core::classrl(Core::ExtendedSourceRegister src) noexcept {
-        //TODO implement
-		auto val = src.get<ExtendedReal>();
-		auto s = (val.sign << 3) & 0b1000;
-		if (val.isZero()) {
-			_ac.arithmeticStatusField = s | 0b000;
-		} else if (val.isDenormal()) {
-			_ac.arithmeticStatusField = s | 0b001;
-		} else if (val.isNormal()) {
-			_ac.arithmeticStatusField = s | 0b010;
-		} else if (val.isInfinity()) {
-			_ac.arithmeticStatusField = s | 0b011;
-		} else if (val.isQuietNaN()) {
-			_ac.arithmeticStatusField = s | 0b100;
-		} else if (val.isSignalingNaN()) {
-			_ac.arithmeticStatusField = s | 0b101;
-		} else if (val.isReservedEncoding()) {
-			_ac.arithmeticStatusField = s | 0b110;
-		}
+		performClassification<decltype(src), ExtendedReal>(src, _ac);
     }
 #undef __DEFAULT_TWO_ARGS__
 #undef __DEFAULT_DOUBLE_WIDE_TWO_ARGS__
