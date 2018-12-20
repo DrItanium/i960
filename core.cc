a7b93b1d6ca576dda75d3a11cdf66c800f6151df
Joshua Scoggins <Theoretically.x64@gmail.com>
2018-12-19 18:29:20 -0800

    Rearrange auto generated stuff at bottom

--- core.cc
+++ core.cc
@@ -554,17 +554,6 @@ namespace i960 {
 				throw "Undefined operation";
 		}
 	}
-#define X(kind, code) \
-	void Core:: b ## kind (Integer addr) noexcept { branchIfGeneric<ConditionCode:: code > ( addr ) ; } 
-	X(e, Equal);
-	X(ne, NotEqual);
-	X(l, LessThan);
-	X(le, LessThanOrEqual);
-	X(g, GreaterThan);
-	X(ge, GreaterThanOrEqual);
-	X(o, Ordered);
-	X(no, Unordered);
-#undef X
 	void Core::faulte() noexcept {
         if (conditionCodeIs<ConditionCode::Equal>()) {
 		    //TODO implement
@@ -638,26 +627,6 @@ namespace i960 {
 			_instructionPointer += 4;
 		}
 	}
-#define X(cmpop, bop) \
-	void Core:: cmpop ## bop ( __TWO_SOURCE_AND_INT_ARGS__ ) noexcept { \
-		cmpop ( src1, src2 ) ; \
-		bop ( targ ) ; \
-	}
-X(cmpo, bg);
-X(cmpo, be);
-X(cmpo, bge);
-X(cmpo, bl);
-X(cmpo, bne);
-X(cmpo, ble);
-X(cmpi, bg);
-X(cmpi, be);
-X(cmpi, bge);
-X(cmpi, bl);
-X(cmpi, bne);
-X(cmpi, ble);
-X(cmpi, bo);
-X(cmpi, bno);
-#undef X
 	void Core::balx(__DEFAULT_TWO_ARGS__) noexcept {
 		// TODO support 4 or 8 byte versions
 		dest.set<Ordinal>(_instructionPointer + 4);
@@ -900,40 +869,6 @@ X(cmpi, bno);
 	void Core::inten() {
 		// TODO implement
 	}
-#define X(kind, mask) \
-	void Core:: sel ## kind (__DEFAULT_THREE_ARGS__) noexcept { \
-		baseSelect<mask>(src1, src2, dest); \
-	}
-	X(no, 0b000);
-	X(g, 0b001);
-	X(e, 0b010);
-	X(ge, 0b011);
-	X(l, 0b100);
-	X(ne, 0b101);
-	X(le, 0b110);
-	X(o, 0b111);
-#undef X
-#define X(base, kind, code) \
-	void Core:: base ## kind ( __DEFAULT_THREE_ARGS__ ) noexcept { \
-		base ## Base <code> ( src1, src2, dest ) ; \
-	}
-	X(subo, no, 0b000); X(subi, no, 0b000);
-	X(subo, g, 0b001);  X(subi, g, 0b001);
-	X(subo, e, 0b010);  X(subi, e, 0b010);
-	X(subo, ge, 0b011); X(subi, ge, 0b011);
-	X(subo, l, 0b100); X(subi, l, 0b100);
-	X(subo, ne, 0b101); X(subi, ne, 0b101);
-	X(subo, le, 0b110); X(subi, le, 0b110);
-	X(subo, o, 0b111); X(subi, o, 0b111);
-	X(addo, no, 0b000); X(addi, no, 0b000);
-	X(addo, g, 0b001);  X(addi, g, 0b001);
-	X(addo, e, 0b010);  X(addi, e, 0b010);
-	X(addo, ge, 0b011); X(addi, ge, 0b011);
-	X(addo, l, 0b100); X(addi, l, 0b100);
-	X(addo, ne, 0b101); X(addi, ne, 0b101);
-	X(addo, le, 0b110); X(addi, le, 0b110);
-	X(addo, o, 0b111); X(addi, o, 0b111);
-#undef X
 
 	void Core::halt(SourceRegister src1) {
 		// TODO finish implementing this
@@ -1011,9 +946,80 @@ X(cmpi, bno);
 		// extended shift right ordinal
 		
 	}
-	void Core::icctl(__DEFAULT_THREE_ARGS__) noexcept { }
-	void Core::intctl(__DEFAULT_TWO_ARGS__) { }
-	void Core::sysctl(__DEFAULT_THREE_ARGS__) noexcept { }
+	void Core::icctl(__DEFAULT_THREE_ARGS__) noexcept { 
+		// TODO implement
+	}
+	void Core::intctl(__DEFAULT_TWO_ARGS__) { 
+		// TODO: implement
+	}
+	void Core::sysctl(__DEFAULT_THREE_ARGS__) noexcept { 
+		// TODO: implement
+	}
+#define X(kind, code) \
+	void Core:: b ## kind (Integer addr) noexcept { branchIfGeneric<ConditionCode:: code > ( addr ) ; } 
+	X(e, Equal);
+	X(ne, NotEqual);
+	X(l, LessThan);
+	X(le, LessThanOrEqual);
+	X(g, GreaterThan);
+	X(ge, GreaterThanOrEqual);
+	X(o, Ordered);
+	X(no, Unordered);
+#undef X
+#define X(kind, mask) \
+	void Core:: sel ## kind (__DEFAULT_THREE_ARGS__) noexcept { \
+		baseSelect<mask>(src1, src2, dest); \
+	}
+	X(no, 0b000);
+	X(g, 0b001);
+	X(e, 0b010);
+	X(ge, 0b011);
+	X(l, 0b100);
+	X(ne, 0b101);
+	X(le, 0b110);
+	X(o, 0b111);
+#undef X
+#define X(base, kind, code) \
+	void Core:: base ## kind ( __DEFAULT_THREE_ARGS__ ) noexcept { \
+		base ## Base <code> ( src1, src2, dest ) ; \
+	}
+	X(subo, no, 0b000); X(subi, no, 0b000);
+	X(subo, g, 0b001);  X(subi, g, 0b001);
+	X(subo, e, 0b010);  X(subi, e, 0b010);
+	X(subo, ge, 0b011); X(subi, ge, 0b011);
+	X(subo, l, 0b100); X(subi, l, 0b100);
+	X(subo, ne, 0b101); X(subi, ne, 0b101);
+	X(subo, le, 0b110); X(subi, le, 0b110);
+	X(subo, o, 0b111); X(subi, o, 0b111);
+	X(addo, no, 0b000); X(addi, no, 0b000);
+	X(addo, g, 0b001);  X(addi, g, 0b001);
+	X(addo, e, 0b010);  X(addi, e, 0b010);
+	X(addo, ge, 0b011); X(addi, ge, 0b011);
+	X(addo, l, 0b100); X(addi, l, 0b100);
+	X(addo, ne, 0b101); X(addi, ne, 0b101);
+	X(addo, le, 0b110); X(addi, le, 0b110);
+	X(addo, o, 0b111); X(addi, o, 0b111);
+#undef X
+#define X(cmpop, bop) \
+	void Core:: cmpop ## bop ( __TWO_SOURCE_AND_INT_ARGS__ ) noexcept { \
+		cmpop ( src1, src2 ) ; \
+		bop ( targ ) ; \
+	}
+X(cmpo, bg);
+X(cmpo, be);
+X(cmpo, bge);
+X(cmpo, bl);
+X(cmpo, bne);
+X(cmpo, ble);
+X(cmpi, bg);
+X(cmpi, be);
+X(cmpi, bge);
+X(cmpi, bl);
+X(cmpi, bne);
+X(cmpi, ble);
+X(cmpi, bo);
+X(cmpi, bno);
+#undef X
 #undef __DEFAULT_TWO_ARGS__
 #undef __DEFAULT_DOUBLE_WIDE_TWO_ARGS__
 #undef __DEFAULT_THREE_ARGS__
