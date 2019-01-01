12566d3301565b0390bb4d82bde5cd44cae403a7
Joshua Scoggins <Theoretically.x64@gmail.com>
2019-01-01 15:43:55 -0800

    Define the bus and bus control units

--- bus.h
+++ bus.h
@@ -1,6 +1,22 @@
 #ifndef I960_BUS_H__
 #define I960_BUS_H__
 namespace i960 {
-
+	/**
+	 * The bus control unit is how the processor interfaces with the rest of
+	 * the system.
+	 */
+	class BusControlUnit {
+	};
+	class Bus {
+		public:
+			enum class State {
+				Idle,
+				Address,
+				Wait_Data,
+				Recovery,
+				Hold,
+				Once,
+			};
+	};
 } // end namespace i960
 #endif // end I960_BUS_H__
