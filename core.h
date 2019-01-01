819e7a205bcaa8d1b76b33e29f4618935388eb5f
Joshua Scoggins <Theoretically.x64@gmail.com>
2018-12-31 18:28:31 -0800

    Added more field accessing to CoreInformation

--- core.h
+++ core.h
@@ -96,17 +96,24 @@ namespace i960 {
 					Ordinal devId, 
 					CoreVoltageKind voltage, 
 					Ordinal icacheSize, 
-					Ordinal dcacheSize) :
+					Ordinal dcacheSize) noexcept :
 				_str(str), 
 				_devId(devId), 
 				_voltage(voltage), 
 				_icacheSize(icacheSize), 
-				_dcacheSize(dcacheSize) { }
+				_dcacheSize(dcacheSize) { 
+
+				}
 			constexpr auto getString() const noexcept { return _str; }
 			constexpr auto getDeviceId() const noexcept { return _devId; }
 			constexpr auto getVoltage() const noexcept { return _voltage; }
 			constexpr auto getInstructionCacheSize() const noexcept { return _icacheSize; }
 			constexpr auto getDataCacheSize() const noexcept { return _dcacheSize; }
+			constexpr auto getVersion() const noexcept { return (_devId & 0xF000'0000) >> 28; }
+			constexpr auto getProductType() const noexcept { return (_devId & 0x0FE0'0000) >> 21; }
+			constexpr auto getGeneration() const noexcept { return (_devId & 0x001E'0000) >> 17; }
+			constexpr auto getModel() const noexcept { return (_devId & 0x0001'F000) >> 12; }
+			constexpr auto getManufacturer() const noexcept { return (_devId & 0x0000'0FFE) >> 1; }
 		private:
 			const char* _str;
 			Ordinal _devId;
@@ -116,6 +123,10 @@ namespace i960 {
 	};
 	constexpr CoreInformation cpu80L960JA("80L960JA", 0x0082'1013, CoreInformation::CoreVoltageKind::V3_3, 2048u, 1024u);
 	constexpr CoreInformation cpu80960JF("80960JF", 0x0882'0013, CoreInformation::CoreVoltageKind::V5_0, 4096u, 2048u);
+	static_assert(cpu80960JF.getGeneration() == 0b0001, "Bad generation check!");
+	static_assert(cpu80960JF.getModel() == 0, "Bad model check!");
+	static_assert(cpu80960JF.getProductType() == 0b1000100, "Bad product type check!");
+	static_assert(cpu80960JF.getManufacturer() == 0b0000'0001'001, "Bad manufacturer check!");
 	constexpr CoreInformation cpu80L960JF("80L960JF", 0x0082'0013, CoreInformation::CoreVoltageKind::V3_3, 4096u, 2048u);
 	constexpr CoreInformation cpu80960JD("80960JD", 0x0882'0013, CoreInformation::CoreVoltageKind::V5_0, 4096u, 2048u);
 
