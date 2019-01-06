9b07b5a777c874cdc690ad504befff34923b8168
Joshua Scoggins <Theoretically.x64@gmail.com>
2019-01-06 13:59:51 -0800

    Reduce temporary lifetime in baseSelect

--- core.h
+++ core.h
@@ -335,8 +335,7 @@ namespace i960 {
 			}
 			template<Ordinal mask>
 			void baseSelect(__DEFAULT_THREE_ARGS__) noexcept {
-				SourceRegister sel = genericCondCheck<mask>() ? src2 : src1;
-				dest.set<Ordinal>(sel.get<Ordinal>());
+				dest.set<Ordinal>((genericCondCheck<mask>() ? src2 : src1).get<Ordinal>());
 			}
 			template<Ordinal mask>
 			void addoBase(__DEFAULT_THREE_ARGS__) noexcept {
