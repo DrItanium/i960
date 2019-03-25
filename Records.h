bab9e3432d6c58db7f2f85f9da04818972c83322
Josh Scoggins <jscoggins@parasoft.com>
2019-03-25 14:57:24 -0700

    Implemented parts of the fault table and entries

--- Records.h
+++ Records.h
@@ -50,5 +50,37 @@ namespace i960 {
         // I see 
     } __attribute__((packed));
 
+    struct FaultTableEntry {
+        struct {
+            Ordinal entryType : 2;
+            Ordinal faultHandlerAddress : 30;
+        } __attribute__((packed));
+        Ordinal magicNumber;
+        bool isLocalCallEntry() const noexcept { return entryType == 0b00; }
+        bool isSystemCallEntry() const noexcept { return (entryType == 0b10) && (magicNumber == 0x0000'027F); }
+    } __attribute__((packed));
+
+    struct FaultTable {
+        enum class Index {
+            Override = 0,
+            Parallel = Override,
+            Trace = 1,
+            Operation = 2, 
+            Arithmetic = 3,
+            Constraint = 5,
+            Protection = 7,
+            Type = 10,
+        };
+        FaultTableEntry entries[32];
+        FaultTableEntry& getOverrideFaultEntry() noexcept { return entries[Index::Override]; }
+        FaultTableEntry& getParallelFaultEntry() noexcept { return entries[Index::Parallel]; }
+        FaultTableEntry& getTraceFaultEntry() noexcept { return entries[Index::Trace]; }
+        FaultTableEntry& getOperationFaultEntry() noexcept { return entries[Index::Operation]; }
+        FaultTableEntry& getArithmeticFaultEntry() noexcept { return entries[Index::Arithmetic]; }
+        FaultTableEntry& getConstraintFaultEntry() noexcept { return entries[Index::Constraint]; }
+        FaultTableEntry& getProtectionFaultEntry() noexcept { return entries[Index::Protection]; }
+        FaultTableEntry& getTypeFaultEntry() noexcept { return entries[Index::Type]; }
+    } __attribute__((packed));
+
 } // end namespace i960
 #endif // end I960_RECORDS_H__
