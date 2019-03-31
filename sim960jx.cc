64f91056e8a892ba1c96d1794650f4a584675a23
Joshua Scoggins <Theoretically.x64@gmail.com>
2019-03-31 14:25:05 -0700

    Greatly compress the body of testInstructionEncoding

--- sim960jx.cc
+++ sim960jx.cc
@@ -8,30 +8,22 @@
 #include "opcodes.h"
 #include <string>
 
-constexpr auto mem1G = 0x3FFF'FFFF + 1;
+constexpr i960::Ordinal mem1G = 0x3FFF'FFFF + 1;
 void bootupMessage(std::ostream& os) {
 	os << "Intel 80960JX Simulator" << std::endl;
-	std::cout << std::endl; 
-	std::cout << std::endl; 
-}
-template<typename T>
-bool testInstructionResult(T expected, T value, const std::string& instruction) {
-	if (expected != value) {
-		std::cout << "Testing instruction: " << instruction << " expected: " << std::dec << expected;
-		std::cout << " and got " << std::dec << value << " result: FAIL" << std::endl;
-		return false;
-	} 
-	return true;
+	os << std::endl; 
+	os << std::endl; 
 }
 
-void outputTypeInformation() {
-	std::cout << "Printing out system type sizes for i960 data types:" << std::endl;
-	std::cout << "sizeof(NormalRegister): " << sizeof(i960::NormalRegister) << std::endl;
-	std::cout << "sizeof(ArithmeticControls): " << sizeof(i960::ArithmeticControls) << std::endl;
-	std::cout << "sizeof(Instruction): " << sizeof(i960::Instruction) << std::endl;
+void outputTypeInformation(std::ostream& os) {
+	os << "Printing out system type sizes for i960 data types:" << std::endl;
+	os << "sizeof(NormalRegister): " << sizeof(i960::NormalRegister) << std::endl;
+	os << "sizeof(ArithmeticControls): " << sizeof(i960::ArithmeticControls) << std::endl;
+	os << "sizeof(Instruction): " << sizeof(i960::Instruction) << std::endl;
 }
 
-void testOverflowOfDisplacement() noexcept {
+void testOverflowOfDisplacement(std::ostream& os) noexcept {
+	constexpr i960::Ordinal _base = 0xFF00'0000;
     union {
         i960::Integer _displacement : 24;
         i960::Integer _value;
@@ -39,49 +31,29 @@ void testOverflowOfDisplacement() noexcept {
     converter._value = 0;
     converter._displacement = -8388608;
 
-    std::cout << "value: " << std::dec << converter._value << ", displacement: " << std::dec << converter._displacement << std::endl;
-	i960::Ordinal _base = 0xFF00'0000;
-	std::cout << "Base value: " << _base << std::endl;
-	std::cout << "Combined value: " << (_base + converter._displacement) << std::endl;
-	std::cout << "Combined value is less: " << std::boolalpha << ((_base + converter._displacement) < _base) << std::endl;
+    os << "value: " << std::dec << converter._value << ", displacement: " << std::dec << converter._displacement << std::endl;
+	os << "Base value: " << _base << std::endl;
+	os << "Combined value: " << (_base + converter._displacement) << std::endl;
+	os << "Combined value is less: " << std::boolalpha << ((_base + converter._displacement) < _base) << std::endl;
 }
-bool testInstructionEncoding(const i960::Instruction& test, const std::string& compareAgainst) noexcept {
-	std::cout << "test encoding: " << std::hex << test._raw << std::endl;
-	std::cout << "\tbase opcode: 0x" << std::hex << test.getOpcode() << std::endl;
-	std::cout << "\tencoded format for mema: " << std::hex << test._mem._mema._offset << std::endl;
+bool testInstructionEncoding(std::ostream& os, const i960::Instruction& test, const std::string& compareAgainst) noexcept {
+	os << "test encoding: " << std::hex << test._raw << std::endl;
+	os << "\tbase opcode: 0x" << std::hex << test.getOpcode() << std::endl;
+	os << "\tencoded format for mema: " << std::hex << test._mem._mema._offset << std::endl;
 	if (auto description = i960::Opcode::getDescription(test.getOpcode()); description.isUndefined()) {
-		std::cout << "\tInstruction mnemonic: 'undefined'" << std::endl;
+		os << "\tInstruction mnemonic: 'undefined'" << std::endl;
 		return false;
 	} else {
 		std::string tmp(description.getString());
-		std::cout << "\tInstruction mnemonic: '" << tmp << "'" << std::endl;
+		os << "\tInstruction mnemonic: '" << tmp << "'" << std::endl;
 		return tmp == compareAgainst;
 	}
 }
-void testInstructionEncoding() {
-	using Instruction = i960::Instruction;
-	auto fn = [](i960::Ordinal opcode, const std::string& compareAgainst) {
-		Instruction tmp(0);
-		if (opcode > 0xFF) {
-			auto upperOpcode = (0xFF0 & opcode) >> 4;
-			auto lowerOpcode = (0x00F & opcode) ;
-			tmp._reg._opcode = upperOpcode;
-			tmp._reg._opcode2 = lowerOpcode;
-		} else {
-			// non register kind
-			tmp._raw = opcode << 24;
-		}
-		return testInstructionEncoding(tmp, compareAgainst);
-	};
+void testInstructionEncoding(std::ostream& os) {
+	constexpr i960::Ordinal instructions[] = {
 #define X(name, code) \
-	std::cout << "Decoding: " << #name << std::endl; \
-	if (fn(code, #name)) { \
-		std::cout << "Outcome: successful!" << std::endl; \
-	} else { \
-		std::cout << "Outcome: failed!" << std::endl; \
-		return; \
-	}
-#define reg(name, code, __) X(name, code);
+		code,
+#define reg(name, code, __) X(name, code)
 #define mem(name, code, __) X(name, code)
 #define cobr(name, code, __) X(name, code)
 #define ctrl(name, code, __) X(name, code)
@@ -91,13 +63,36 @@ void testInstructionEncoding() {
 #undef mem
 #undef cobr
 #undef ctrl
+	};
+	for (auto i : instructions) {
+		auto name = i960::Opcode::getDescription(i).getString();
+		auto opcode = i;
+		os << "Decoding: " << name << std::endl;
+		os << "Outcome: ";
+		i960::Instruction tmp(0);
+		if (opcode > 0xFF) {
+			auto upperOpcode = (0xFF0 & opcode) >> 4;
+			auto lowerOpcode = (0x00F & opcode) ;
+			tmp._reg._opcode = upperOpcode;
+			tmp._reg._opcode2 = lowerOpcode;
+		} else {
+			// non register kind
+			tmp._raw = opcode << 24;
+		}
+		if (testInstructionEncoding(os, tmp, name)) {
+			os << "successful!" << std::endl;
+		} else {
+			os << "failed!" << std::endl;
+			return;
+		}
+	}
 }
 int main() {
 	int errorCode = 0;
 	bootupMessage(std::cout);
-	outputTypeInformation();
-    testOverflowOfDisplacement();
-	testInstructionEncoding();
+	outputTypeInformation(std::cout);
+    testOverflowOfDisplacement(std::cout);
+	testInstructionEncoding(std::cout);
 	std::cout << "Shutting down..." << std::endl;
 
 	return errorCode;
