c8bb3d3200da35bb92034f7a72037473ba92755a
Joshua Scoggins <Theoretically.x64@gmail.com>
2019-06-23 13:20:27 -0700

    Generate descriptions at constexpr time

--- opcodes.h
+++ opcodes.h
@@ -149,5 +149,25 @@ namespace i960::Opcode {
             return getDescription(inst.getOpcode());
         }
 		const Description& getDescription(Ordinal opcode) noexcept;
+        TargetOpcode determineTargetOpcode(const Instruction& inst) noexcept {
+            return determineTargetOpcode(inst.getOpcode());
+        }
+        constexpr TargetOpcode determineTargetOpcode(Ordinal opcode) noexcept {
+            switch (opcode) {
+#define body(name, code, arg, kind) case code : return name ## Description () ;
+#define reg(name, code, arg)  body(name, code, arg, Reg)
+#define cobr(name, code, arg)  body(name, code, arg, Cobr)
+#define mem(name, code, arg)  body(name, code, arg, Mem)
+#define ctrl(name, code, arg)  body(name, code, arg, Ctrl)
+#include "opcodes.def"
+#undef reg
+#undef cobr
+#undef mem
+#undef ctrl
+#undef body
+                default:
+                    return UndefinedDescription();
+            }
+        }
 } // end namespace i960::Opcode
 #endif // end I960_OPCODES_H__
