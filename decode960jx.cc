1c4723cdcd4d8da9ba704d7f8aba86ecdec86fa3
Joshua Scoggins <Theoretically.x64@gmail.com>
2019-06-23 12:00:06 -0700

    Disable code in a hack and slash fashion to get compilation

--- decode960jx.cc
+++ decode960jx.cc
@@ -6,136 +6,136 @@
 #include "Instruction.h"
 #include <string>
 #include <sstream>
-void decode(std::ostream& out, const i960::Opcode::Description& desc, const i960::Instruction::REGFormat& inst) {
-    if (desc.hasZeroArguments()) {
-        return;
-    }
-    i960::Operand src1(inst.decodeSrc1());
-    i960::Operand src2(inst.decodeSrc2());
-    i960::Operand srcDest(inst.decodeSrcDest());
-    out << " " << src1;
-    if (desc.hasOneArgument()) {
-        // we are done now
-        return;
-    }
-    if (desc.hasTwoArguments()) {
-        out << ", ";
-        switch (desc) {
-            case i960::Opcode::cmpos:
-            case i960::Opcode::cmpob:
-            case i960::Opcode::cmpis:
-            case i960::Opcode::cmpib:
-            case i960::Opcode::scanbyte:
-            case i960::Opcode::bswap:
-                out << src2;
-                break;
-            default:
-                out << srcDest;
-                break;
-        }
-    } else if (desc.hasThreeArguments()) {
-        out << ", " << src2 << ", " << srcDest;
-    } else {
-        // we need to state that the instruction is seemingly malformed
-        out << " could not decode rest!";
-    }
-}
-void decode(std::ostream& out, const i960::Opcode::Description& desc, const i960::Instruction::COBRFormat& inst) {
-    i960::Operand src1(inst.decodeSrc1());
-    out << " " << src1;
-    if (!desc.hasOneArgument()) {
-        i960::Operand src2(inst.decodeSrc1());
-        out << ", " << src2 << ", " << inst.decodeDisplacement();
-    }
-}
-void decode(std::ostream& out, const i960::Opcode::Description& desc, const i960::Instruction::MemFormat::MEMBFormat& inst) {
-    out << "0x";
-    using E = std::decay_t<decltype(inst)>::AddressingModes;
-    auto index = inst._index;
-    auto scale = inst.getScaleFactor();
-    i960::Ordinal displacement = 0u;
-    if (inst.has32bitDisplacement()) {
-        std::cin >> std::hex >> displacement;
-        if (!std::cin.good()) {
-            out << "Bad input!!!" << std::endl;
-            return;
-        } else {
-            out << std::hex << displacement << " ";
-        }
-    }
-    out << std::hex << inst._raw << ": " << desc.getString() << " ";
-    i960::Operand abase(inst.decodeAbase());
-    switch(inst.getAddressingMode()) {
-        case E::Abase:
-            out << "(" << abase << ")";
-            break;
-        case E::IP_Plus_Displacement_Plus_8:
-            out << "((ip) + " << displacement << " + 8)";
-            break;
-        case E::Abase_Plus_Index_Times_2_Pow_Scale:
-            out << "((" << abase << ") + (" << index << ") * " << scale << ")"; 
-            break;
-        case E::Displacement:
-            out << displacement;
-            break;
-        case E::Abase_Plus_Displacement:
-            out << "((" << abase << ") + " << displacement << ")";
-            break;
-        case E::Index_Times_2_Pow_Scale_Plus_Displacement:
-            out << "((" << index << ") * " << scale << " + " << displacement << ")";
-            break;
-        case E::Abase_Plus_Index_Times_2_Pow_Scale_Plus_Displacement:
-            out << "((" << abase << ") + (" << index << ") * " << scale << " + " << displacement << ")"; 
-            break;
-        default:
-            out << "ERROR: reserved!!!";
-            break;
-    }
-    out << ", " << i960::Operand(inst.decodeSrcDest());
-}
-void decode(std::ostream& out, const i960::Opcode::Description& desc, const i960::Instruction::MemFormat& inst) {
-    if (inst.isMemAFormat()) {
-        auto ma = inst._mema;
-        i960::Operand srcDest(inst.decodeSrcDest());
-        out << std::hex << "0x" << ma._raw << ": " << desc.getString() << " 0x" << ma._offset;
-        if (!ma.isOffsetAddressingMode()) {
-            out << "(" << i960::Operand(ma.decodeAbase()) << ")";
-        }
-        out << ", " << srcDest;
-    } else {
-        decode(out, desc, inst._memb);
-    }
-}
-std::string decode(i960::Ordinal value) noexcept {
-    i960::Instruction inst(value);
-    auto desc = i960::Opcode::getDescription(inst.getOpcode());
-    std::ostringstream out;
-    if (inst.isRegFormat()) {
-        out << std::hex << "0x" << value << ": " << desc.getString();
-        decode(out, desc, inst._reg);
-    } else if (inst.isControlFormat()) {
-        out << std::hex << "0x" << value << ": " << desc.getString();
-        out << " " << inst._ctrl.decodeDisplacement();
-    } else if (inst.isCompareAndBranchFormat()) { 
-        out << std::hex << "0x" << value << ": " << desc.getString();
-        decode(out, desc, inst._cobr);
-    } else if (inst.isMemFormat()) {
-        decode(out, desc, inst._mem);
-    } else {
-        out << " unknown instruction class... unable to decode further!";
-    }
-    std::string tmp(out.str());
-    return tmp;
-}
+//void decode(std::ostream& out, const i960::Opcode::Description& desc, const i960::Instruction::REGFormat& inst) {
+//    if (desc.hasZeroArguments()) {
+//        return;
+//    }
+//    i960::Operand src1(inst.decodeSrc1());
+//    i960::Operand src2(inst.decodeSrc2());
+//    i960::Operand srcDest(inst.decodeSrcDest());
+//    out << " " << src1;
+//    if (desc.hasOneArgument()) {
+//        // we are done now
+//        return;
+//    }
+//    if (desc.hasTwoArguments()) {
+//        out << ", ";
+//        switch (desc) {
+//            case i960::Opcode::cmpos:
+//            case i960::Opcode::cmpob:
+//            case i960::Opcode::cmpis:
+//            case i960::Opcode::cmpib:
+//            case i960::Opcode::scanbyte:
+//            case i960::Opcode::bswap:
+//                out << src2;
+//                break;
+//            default:
+//                out << srcDest;
+//                break;
+//        }
+//    } else if (desc.hasThreeArguments()) {
+//        out << ", " << src2 << ", " << srcDest;
+//    } else {
+//        // we need to state that the instruction is seemingly malformed
+//        out << " could not decode rest!";
+//    }
+//}
+//void decode(std::ostream& out, const i960::Opcode::Description& desc, const i960::Instruction::COBRFormat& inst) {
+//    i960::Operand src1(inst.decodeSrc1());
+//    out << " " << src1;
+//    if (!desc.hasOneArgument()) {
+//        i960::Operand src2(inst.decodeSrc1());
+//        out << ", " << src2 << ", " << inst.decodeDisplacement();
+//    }
+//}
+//void decode(std::ostream& out, const i960::Opcode::Description& desc, const i960::Instruction::MemFormat::MEMBFormat& inst) {
+//    out << "0x";
+//    using E = std::decay_t<decltype(inst)>::AddressingModes;
+//    auto index = inst._index;
+//    auto scale = inst.getScaleFactor();
+//    i960::Ordinal displacement = 0u;
+//    if (inst.has32bitDisplacement()) {
+//        std::cin >> std::hex >> displacement;
+//        if (!std::cin.good()) {
+//            out << "Bad input!!!" << std::endl;
+//            return;
+//        } else {
+//            out << std::hex << displacement << " ";
+//        }
+//    }
+//    out << std::hex << inst._raw << ": " << desc.getString() << " ";
+//    i960::Operand abase(inst.decodeAbase());
+//    switch(inst.getAddressingMode()) {
+//        case E::Abase:
+//            out << "(" << abase << ")";
+//            break;
+//        case E::IP_Plus_Displacement_Plus_8:
+//            out << "((ip) + " << displacement << " + 8)";
+//            break;
+//        case E::Abase_Plus_Index_Times_2_Pow_Scale:
+//            out << "((" << abase << ") + (" << index << ") * " << scale << ")"; 
+//            break;
+//        case E::Displacement:
+//            out << displacement;
+//            break;
+//        case E::Abase_Plus_Displacement:
+//            out << "((" << abase << ") + " << displacement << ")";
+//            break;
+//        case E::Index_Times_2_Pow_Scale_Plus_Displacement:
+//            out << "((" << index << ") * " << scale << " + " << displacement << ")";
+//            break;
+//        case E::Abase_Plus_Index_Times_2_Pow_Scale_Plus_Displacement:
+//            out << "((" << abase << ") + (" << index << ") * " << scale << " + " << displacement << ")"; 
+//            break;
+//        default:
+//            out << "ERROR: reserved!!!";
+//            break;
+//    }
+//    out << ", " << i960::Operand(inst.decodeSrcDest());
+//}
+//void decode(std::ostream& out, const i960::Opcode::Description& desc, const i960::Instruction::MemFormat& inst) {
+//    if (inst.isMemAFormat()) {
+//        auto ma = inst._mema;
+//        i960::Operand srcDest(inst.decodeSrcDest());
+//        out << std::hex << "0x" << ma._raw << ": " << desc.getString() << " 0x" << ma._offset;
+//        if (!ma.isOffsetAddressingMode()) {
+//            out << "(" << i960::Operand(ma.decodeAbase()) << ")";
+//        }
+//        out << ", " << srcDest;
+//    } else {
+//        decode(out, desc, inst._memb);
+//    }
+//}
+//std::string decode(i960::Ordinal value) noexcept {
+//    i960::Instruction inst(value);
+//    auto desc = i960::Opcode::getDescription(inst.getOpcode());
+//    std::ostringstream out;
+//    if (inst.isRegFormat()) {
+//        out << std::hex << "0x" << value << ": " << desc.getString();
+//        decode(out, desc, inst._reg);
+//    } else if (inst.isControlFormat()) {
+//        out << std::hex << "0x" << value << ": " << desc.getString();
+//        out << " " << inst._ctrl.decodeDisplacement();
+//    } else if (inst.isCompareAndBranchFormat()) { 
+//        out << std::hex << "0x" << value << ": " << desc.getString();
+//        decode(out, desc, inst._cobr);
+//    } else if (inst.isMemFormat()) {
+//        decode(out, desc, inst._mem);
+//    } else {
+//        out << " unknown instruction class... unable to decode further!";
+//    }
+//    std::string tmp(out.str());
+//    return tmp;
+//}
 int main() {
 	int errorCode = 0;
-    while(std::cin.good()) {
-        std::cout << "> ";
-        i960::Ordinal value = 0;
-        std::cin >> std::hex >> value;
-        if (std::cin.good()) {
-            std::cout << decode(value) << std::endl;
-        }
-    }
+    //while(std::cin.good()) {
+    //    std::cout << "> ";
+    //    i960::Ordinal value = 0;
+    //    std::cin >> std::hex >> value;
+    //    if (std::cin.good()) {
+    //        std::cout << decode(value) << std::endl;
+    //    }
+    //}
 	return errorCode;
 }
--- dispatcher.cc
+++ dispatcher.cc
@@ -8,198 +8,198 @@
 
 namespace i960 {
 	void Core::dispatch(const Instruction& inst) noexcept {
-		auto selectRegister = [this](const Operand& operand, NormalRegister& imm) -> NormalRegister& {
-			if (operand.isLiteral()) {
-				imm.set(operand.getValue());
-				return imm;
-			} else {
-				return getRegister(operand);
-			}
-		};
-		if (auto desc = Opcode::getDescription(inst); desc.isUndefined()) {
-			generateFault(OperationFaultSubtype::InvalidOpcode);
-		} else if (desc.hasZeroArguments()) {
-			// these operations require no further decoding effort so short
-			// circuit them :D
-			switch (desc) {
-#define Y(kind) case Opcode:: kind :  kind () ; break;
-#define X(kind, __) Y(fault ## kind );
-				Y(mark);  Y(fmark); Y(flushreg); 
-				Y(syncf); Y(ret);   Y(inten);
-				Y(intdis);
-#include "conditional_kinds.def"
-#undef X
-#undef Y
-                default: 
-                    generateFault(OperationFaultSubtype::InvalidOpcode); 
-                    break;
-			}
-		} else if (desc.isReg()) {
-			auto reg = inst._reg;
-			auto opSrc2 = reg.decodeSrc2();
-			auto opSrcDest = reg.decodeSrcDest();
-			NormalRegister& src1 = selectRegister(reg.decodeSrc1(), _temporary0);
-			NormalRegister& src2 = selectRegister(opSrc2, _temporary1);
-			// TODO It is impossible for m3 to be set when srcDest is used as a dest, error out before hand
-			NormalRegister& srcDest = selectRegister(opSrcDest, _temporary2);
-			switch(desc) {
-#define Y(kind, args) case Opcode:: kind : kind args ; break 
-#define Op3Arg(kind) Y(kind, (src1, src2, srcDest))
-#define Op2Arg(kind) Y(kind, (src1, srcDest))
-				Op2Arg(spanbit);  Op2Arg(scanbit);  Op2Arg(opnot);
-				Op2Arg(chkbit);   Op2Arg(cmpo);     Op2Arg(cmpi);    
-				Op2Arg(concmpo);  Op2Arg(concmpi);
-
-				Op3Arg(notbit);  Op3Arg(clrbit);   Op3Arg(notor);   Op3Arg(opand);
-				Op3Arg(andnot);  Op3Arg(setbit);   Op3Arg(notand);  Op3Arg(opxor);
-				Op3Arg(opor);    Op3Arg(nor);      Op3Arg(xnor);    Op3Arg(ornot);
-				Op3Arg(nand);    Op3Arg(alterbit); Op3Arg(shro);    Op3Arg(shrdi);
-				Op3Arg(shri);    Op3Arg(shlo);     Op3Arg(rotate);  Op3Arg(shli);
-				Op3Arg(addc);    Op3Arg(subc);     Op3Arg(atmod);   Op3Arg(atadd);
-				Op3Arg(modify);  Op3Arg(extract);  Op3Arg(modac);   Op3Arg(modtc);
-				Op3Arg(modpc);   Op3Arg(addo);     Op3Arg(addi);    Op3Arg(subo);
-				Op3Arg(subi);    Op3Arg(cmpinco);  Op3Arg(cmpinci); Op3Arg(cmpdeco);
-				Op3Arg(cmpdeci); Op3Arg(mulo);     Op3Arg(muli);    Op3Arg(remo);
-				Op3Arg(remi);    Op3Arg(divo);     Op3Arg(divi);    Op3Arg(dcctl);
-
-
-				Y(scanbyte, (src1, src2));
-                Y(cmpos, (src1, src2));
-                Y(cmpis, (src1, src2));
-                Y(cmpob, (src1, src2));
-                Y(cmpib, (src1, src2));
-                Y(bswap, (src1, src2));
-				Y(mov, (reg.decodeSrc1(), opSrcDest));
-				Y(movl, (reg.decodeSrc1(), opSrcDest));
-				Y(movt, (reg.decodeSrc1(), opSrcDest));
-				Y(movq, (reg.decodeSrc1(), opSrcDest));
-				Y(calls, (src1));
-				Y(eshro, (src1, opSrc2, srcDest));
-				Y(emul, (src1, src2, opSrcDest));
-				Y(ediv, (src1, opSrc2, opSrcDest));
-#define X(kind, __) \
-				Op3Arg(addo ## kind); \
-				Op3Arg(addi ## kind); \
-				Op3Arg(subo ## kind); \
-				Op3Arg(subi ## kind); \
-				Op3Arg(sel ## kind); 
-#include "conditional_kinds.def"
-#undef X
-#undef Op3Arg
-#undef Op2Arg
-#undef Y
-				default: 
-                    generateFault(OperationFaultSubtype::InvalidOpcode); 
-                    break;
-			}
-		} else if (desc.isMem()) {
-			auto mem = inst._mem;
-            InstructionLength length = InstructionLength::Single;
-			if (mem.isMemAFormat()) {
-				auto ma = mem._mema;
-				_temporary0.set<Ordinal>(ma._offset + ma.isOffsetAddressingMode() ?  0 : getRegister(ma.decodeAbase()).get<Ordinal>());
-			} else {
-				auto mb = mem._memb;
-				using E = std::decay_t<decltype(mb)>::AddressingModes;
-				auto index = mb._index;
-				auto scale = mb.getScaleFactor();
-                auto displacement = 0u;
-                if (mb.has32bitDisplacement()) {
-                    displacement = mb.get32bitDisplacement();
-                    length = InstructionLength::Double;
-                }
-				switch (auto abase = getRegister(mb.decodeAbase()); mb.getAddressingMode()) {
-					case E::Abase:
-						_temporary0.move(abase);
-						break;
-					case E::IP_Plus_Displacement_Plus_8:
-						_temporary0.set<Ordinal>(_instructionPointer + displacement + 8);
-						break;
-					case E::Abase_Plus_Index_Times_2_Pow_Scale:
-						_temporary0.set<Ordinal>(abase.get<Ordinal>() + index * scale);
-						break;
-					case E::Displacement:
-						_temporary0.set(displacement);
-						break;
-					case E::Abase_Plus_Displacement:
-						_temporary0.set(displacement + abase.get<Ordinal>());
-						break;
-					case E::Index_Times_2_Pow_Scale_Plus_Displacement:
-						_temporary0.set(index * scale + displacement);
-						break;
-					case E::Abase_Plus_Index_Times_2_Pow_Scale_Plus_Displacement:
-						_temporary0.set(abase.get<Ordinal>() + index * scale + displacement);
-						break;
-					default: 
-						generateFault(OperationFaultSubtype::InvalidOpcode); 
-                        return;
-				}
-			}
-			// opcode does not differentiate between mema and memb, another bit
-			// does, so the actual arguments differ, not the set of actions
-			switch(desc) {
-                case Opcode::balx:
-                    balx(_temporary0, getRegister(mem.decodeSrcDest()), length);
-                    break;
-
-#define Y(kind, a, b) case Opcode:: kind : kind ( a , b ) ; break;
-#define YISSD(kind) Y(kind, _temporary0, getRegister(mem.decodeSrcDest()));
-#define YSDIS(kind) Y(kind, getRegister(mem.decodeSrcDest()), _temporary0);
-#define ZIS(kind) case Opcode:: kind : kind ( _temporary0 ) ; break;
-#define LDP(suffix) \
-				YISSD(ld ## suffix); \
-				YSDIS(st ## suffix);
-#define WLDP(suffix) \
-				Y(ld ## suffix, _temporary0, mem.decodeSrcDest()); \
-				Y(st ## suffix, mem.decodeSrcDest(), _temporary0 ); 
-				LDP(ob);     LDP(os);   LDP(ib); LDP(is);
-				WLDP(l);     WLDP(t);   WLDP(q); 
-				YSDIS(st);   YISSD(ld); YISSD(lda); 
-				ZIS(bx);   ZIS(callx);
-#undef ZIS
-#undef YISSD
-#undef YSDIS 
-#undef WLDP
-#undef LDP
-#undef Y
-				default: 
-					generateFault(OperationFaultSubtype::InvalidOpcode); 
-                    return;
-			}
-		} else if (desc.isCtrl()) {
-			switch (desc) {
-#define Y(kind) case Opcode:: kind : kind ( inst._ctrl.decodeDisplacement() ) ; break
-                Y(b);
-                Y(call);
-                Y(bal);
-#define X(kind, __) Y(b ## kind);
-#include "conditional_kinds.def"
-#undef X
-#undef Y
-				default: 
-					generateFault(OperationFaultSubtype::InvalidOpcode); 
-                    return;
-			}
-		} else if (desc.isCobr()) {
-			switch(desc) {
-#define Y(kind) case Opcode:: kind : kind ( selectRegister(inst._cobr.decodeSrc1(), _temporary0), getRegister(inst._cobr.decodeSrc2()), inst._cobr.decodeDisplacement()); break
-#define X(kind) Y( cmpob ## kind ) 
-				Y(bbc); Y(bbs); X(e);
-				X(ge);  X(l);   X(ne);
-				X(le);
-#undef X
-#define X(kind, __) \
-				Y(cmpib ## kind ); \
-				case Opcode:: test ## kind : test ## kind (selectRegister(inst._cobr.decodeSrc1(), _temporary0)); break;
-#include "conditional_kinds.def"
-#undef X
-#undef Y
-				default: 
-					generateFault(OperationFaultSubtype::InvalidOpcode); 
-                    return;
-			}
-		} else {
-			generateFault(OperationFaultSubtype::InvalidOpcode); 
-		}
+//		auto selectRegister = [this](const Operand& operand, NormalRegister& imm) -> NormalRegister& {
+//			if (operand.isLiteral()) {
+//				imm.set(operand.getValue());
+//				return imm;
+//			} else {
+//				return getRegister(operand);
+//			}
+//		};
+//		if (auto desc = Opcode::getDescription(inst); desc.isUndefined()) {
+//			generateFault(OperationFaultSubtype::InvalidOpcode);
+//		} else if (desc.hasZeroArguments()) {
+//			// these operations require no further decoding effort so short
+//			// circuit them :D
+//			switch (desc) {
+//#define Y(kind) case Opcode:: kind :  kind () ; break;
+//#define X(kind, __) Y(fault ## kind );
+//				Y(mark);  Y(fmark); Y(flushreg); 
+//				Y(syncf); Y(ret);   Y(inten);
+//				Y(intdis);
+//#include "conditional_kinds.def"
+//#undef X
+//#undef Y
+//                default: 
+//                    generateFault(OperationFaultSubtype::InvalidOpcode); 
+//                    break;
+//			}
+//		} else if (desc.isReg()) {
+//			auto reg = inst._reg;
+//			auto opSrc2 = reg.decodeSrc2();
+//			auto opSrcDest = reg.decodeSrcDest();
+//			NormalRegister& src1 = selectRegister(reg.decodeSrc1(), _temporary0);
+//			NormalRegister& src2 = selectRegister(opSrc2, _temporary1);
+//			// TODO It is impossible for m3 to be set when srcDest is used as a dest, error out before hand
+//			NormalRegister& srcDest = selectRegister(opSrcDest, _temporary2);
+//			switch(desc) {
+//#define Y(kind, args) case Opcode:: kind : kind args ; break 
+//#define Op3Arg(kind) Y(kind, (src1, src2, srcDest))
+//#define Op2Arg(kind) Y(kind, (src1, srcDest))
+//				Op2Arg(spanbit);  Op2Arg(scanbit);  Op2Arg(opnot);
+//				Op2Arg(chkbit);   Op2Arg(cmpo);     Op2Arg(cmpi);    
+//				Op2Arg(concmpo);  Op2Arg(concmpi);
+//
+//				Op3Arg(notbit);  Op3Arg(clrbit);   Op3Arg(notor);   Op3Arg(opand);
+//				Op3Arg(andnot);  Op3Arg(setbit);   Op3Arg(notand);  Op3Arg(opxor);
+//				Op3Arg(opor);    Op3Arg(nor);      Op3Arg(xnor);    Op3Arg(ornot);
+//				Op3Arg(nand);    Op3Arg(alterbit); Op3Arg(shro);    Op3Arg(shrdi);
+//				Op3Arg(shri);    Op3Arg(shlo);     Op3Arg(rotate);  Op3Arg(shli);
+//				Op3Arg(addc);    Op3Arg(subc);     Op3Arg(atmod);   Op3Arg(atadd);
+//				Op3Arg(modify);  Op3Arg(extract);  Op3Arg(modac);   Op3Arg(modtc);
+//				Op3Arg(modpc);   Op3Arg(addo);     Op3Arg(addi);    Op3Arg(subo);
+//				Op3Arg(subi);    Op3Arg(cmpinco);  Op3Arg(cmpinci); Op3Arg(cmpdeco);
+//				Op3Arg(cmpdeci); Op3Arg(mulo);     Op3Arg(muli);    Op3Arg(remo);
+//				Op3Arg(remi);    Op3Arg(divo);     Op3Arg(divi);    Op3Arg(dcctl);
+//
+//
+//				Y(scanbyte, (src1, src2));
+//                Y(cmpos, (src1, src2));
+//                Y(cmpis, (src1, src2));
+//                Y(cmpob, (src1, src2));
+//                Y(cmpib, (src1, src2));
+//                Y(bswap, (src1, src2));
+//				Y(mov, (reg.decodeSrc1(), opSrcDest));
+//				Y(movl, (reg.decodeSrc1(), opSrcDest));
+//				Y(movt, (reg.decodeSrc1(), opSrcDest));
+//				Y(movq, (reg.decodeSrc1(), opSrcDest));
+//				Y(calls, (src1));
+//				Y(eshro, (src1, opSrc2, srcDest));
+//				Y(emul, (src1, src2, opSrcDest));
+//				Y(ediv, (src1, opSrc2, opSrcDest));
+//#define X(kind, __) \
+//				Op3Arg(addo ## kind); \
+//				Op3Arg(addi ## kind); \
+//				Op3Arg(subo ## kind); \
+//				Op3Arg(subi ## kind); \
+//				Op3Arg(sel ## kind); 
+//#include "conditional_kinds.def"
+//#undef X
+//#undef Op3Arg
+//#undef Op2Arg
+//#undef Y
+//				default: 
+//                    generateFault(OperationFaultSubtype::InvalidOpcode); 
+//                    break;
+//			}
+//		} else if (desc.isMem()) {
+//			auto mem = inst._mem;
+//            InstructionLength length = InstructionLength::Single;
+//			if (mem.isMemAFormat()) {
+//				auto ma = mem._mema;
+//				_temporary0.set<Ordinal>(ma._offset + ma.isOffsetAddressingMode() ?  0 : getRegister(ma.decodeAbase()).get<Ordinal>());
+//			} else {
+//				auto mb = mem._memb;
+//				using E = std::decay_t<decltype(mb)>::AddressingModes;
+//				auto index = mb._index;
+//				auto scale = mb.getScaleFactor();
+//                auto displacement = 0u;
+//                if (mb.has32bitDisplacement()) {
+//                    displacement = mb.get32bitDisplacement();
+//                    length = InstructionLength::Double;
+//                }
+//				switch (auto abase = getRegister(mb.decodeAbase()); mb.getAddressingMode()) {
+//					case E::Abase:
+//						_temporary0.move(abase);
+//						break;
+//					case E::IP_Plus_Displacement_Plus_8:
+//						_temporary0.set<Ordinal>(_instructionPointer + displacement + 8);
+//						break;
+//					case E::Abase_Plus_Index_Times_2_Pow_Scale:
+//						_temporary0.set<Ordinal>(abase.get<Ordinal>() + index * scale);
+//						break;
+//					case E::Displacement:
+//						_temporary0.set(displacement);
+//						break;
+//					case E::Abase_Plus_Displacement:
+//						_temporary0.set(displacement + abase.get<Ordinal>());
+//						break;
+//					case E::Index_Times_2_Pow_Scale_Plus_Displacement:
+//						_temporary0.set(index * scale + displacement);
+//						break;
+//					case E::Abase_Plus_Index_Times_2_Pow_Scale_Plus_Displacement:
+//						_temporary0.set(abase.get<Ordinal>() + index * scale + displacement);
+//						break;
+//					default: 
+//						generateFault(OperationFaultSubtype::InvalidOpcode); 
+//                        return;
+//				}
+//			}
+//			// opcode does not differentiate between mema and memb, another bit
+//			// does, so the actual arguments differ, not the set of actions
+//			switch(desc) {
+//                case Opcode::balx:
+//                    balx(_temporary0, getRegister(mem.decodeSrcDest()), length);
+//                    break;
+//
+//#define Y(kind, a, b) case Opcode:: kind : kind ( a , b ) ; break;
+//#define YISSD(kind) Y(kind, _temporary0, getRegister(mem.decodeSrcDest()));
+//#define YSDIS(kind) Y(kind, getRegister(mem.decodeSrcDest()), _temporary0);
+//#define ZIS(kind) case Opcode:: kind : kind ( _temporary0 ) ; break;
+//#define LDP(suffix) \
+//				YISSD(ld ## suffix); \
+//				YSDIS(st ## suffix);
+//#define WLDP(suffix) \
+//				Y(ld ## suffix, _temporary0, mem.decodeSrcDest()); \
+//				Y(st ## suffix, mem.decodeSrcDest(), _temporary0 ); 
+//				LDP(ob);     LDP(os);   LDP(ib); LDP(is);
+//				WLDP(l);     WLDP(t);   WLDP(q); 
+//				YSDIS(st);   YISSD(ld); YISSD(lda); 
+//				ZIS(bx);   ZIS(callx);
+//#undef ZIS
+//#undef YISSD
+//#undef YSDIS 
+//#undef WLDP
+//#undef LDP
+//#undef Y
+//				default: 
+//					generateFault(OperationFaultSubtype::InvalidOpcode); 
+//                    return;
+//			}
+//		} else if (desc.isCtrl()) {
+//			switch (desc) {
+//#define Y(kind) case Opcode:: kind : kind ( inst._ctrl.decodeDisplacement() ) ; break
+//                Y(b);
+//                Y(call);
+//                Y(bal);
+//#define X(kind, __) Y(b ## kind);
+//#include "conditional_kinds.def"
+//#undef X
+//#undef Y
+//				default: 
+//					generateFault(OperationFaultSubtype::InvalidOpcode); 
+//                    return;
+//			}
+//		} else if (desc.isCobr()) {
+//			switch(desc) {
+//#define Y(kind) case Opcode:: kind : kind ( selectRegister(inst._cobr.decodeSrc1(), _temporary0), getRegister(inst._cobr.decodeSrc2()), inst._cobr.decodeDisplacement()); break
+//#define X(kind) Y( cmpob ## kind ) 
+//				Y(bbc); Y(bbs); X(e);
+//				X(ge);  X(l);   X(ne);
+//				X(le);
+//#undef X
+//#define X(kind, __) \
+//				Y(cmpib ## kind ); \
+//				case Opcode:: test ## kind : test ## kind (selectRegister(inst._cobr.decodeSrc1(), _temporary0)); break;
+//#include "conditional_kinds.def"
+//#undef X
+//#undef Y
+//				default: 
+//					generateFault(OperationFaultSubtype::InvalidOpcode); 
+//                    return;
+//			}
+//		} else {
+//			generateFault(OperationFaultSubtype::InvalidOpcode); 
+//		}
 	}
 } // end namespace i960
