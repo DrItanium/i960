23d11eaf1ca06b7105d9f2392d93f66ac0548883
Joshua Scoggins <Theoretically.x64@gmail.com>
2019-10-19 20:41:32 -0700

    started the arduino sketch for scanning the i960SA's AD lines

---
+++ arduino/i960SA_DataLines_Scanner/80960SA_ADScanner.ino
@@ -0,0 +1,10 @@
+// A mega2560 sketch to inspect the data lines / address lines of the 80960SA
+// processor. This will grow organically
+int dataLines[16] = { 0 };
+
+void setup() {
+}
+
+void loop() {
+
+}
