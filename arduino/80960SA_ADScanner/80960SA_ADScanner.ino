c5711c16d23efead6b115f14f7794cd16f53aa79
Joshua Scoggins <Theoretically.x64@gmail.com>
2019-10-19 20:57:41 -0700

    start with the boot up sequence

--- arduino/80960SA_ADScanner/80960SA_ADScanner.ino
+++ arduino/80960SA_ADScanner/80960SA_ADScanner.ino
@@ -1,10 +1,22 @@
 // A mega2560 sketch to inspect the data lines / address lines of the 80960SA
-// processor. This will grow organically
-int dataLines[16] = { 0 };
-
+// processor. This will grow organically but first lets take a look at the
+// BE1,BE0, and BLAST_ pins
+constexpr auto BLAST = 4;
+constexpr auto BE1 = 3;
+constexpr auto BE0 = 2;
 void setup() {
+    Serial.begin(9600);
+    pinMode(BLAST, INPUT);
+    pinMode(BE1, INPUT);
+    pinMode(BE0, INPUT);
 }
 
 void loop() {
-
+    Serial.print(digitalRead(BLAST));
+    Serial.print(" ");
+    Serial.print(digitalRead(BE0));
+    Serial.print(" ");
+    Serial.print(digitalRead(BE1));
+    Serial.println();
+    delay(1);
 }
