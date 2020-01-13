53bd739d9d8d6f0b851201ff44e191444a32725f
Josh Scoggins <jscoggins@parasoft.com>
2020-01-13 09:09:18 -0800

    Cleanup LogicalMemoryTemplateStartingAddressRegister

--- PMCONRegister.h
+++ PMCONRegister.h
@@ -103,24 +103,40 @@ namespace i960 {
 			bool _internalRAMProtection = false;
 			bool _supervisorInternalRAMProtection = false;
 	};
-	union LogicalMemoryTemplateStartingAddressRegister final {
-		struct {
-			Ordinal _byteOrder : 1;
-			Ordinal _dataCacheEnable : 1;
-			Ordinal _reserved : 10;
-			/**
-			 * Upper 20-bits for the starting address for a logical data
-			 * template. The lower 12 bits are fixed at zero. The starting
-			 * address is modulo 4 kbytes
-			 */
-			Ordinal _templateStartingAddress : 20;
-		};
-		Ordinal _value;
-		constexpr bool littleEndianByteOrder() const noexcept { return _byteOrder == 0; }
-		constexpr bool bigEndianByteOrder() const noexcept { return _byteOrder != 0; }
-		constexpr bool dataCacheEnabled() const noexcept { return _dataCacheEnable != 0; }
-		constexpr Ordinal getTemplateStartingAddress() const noexcept { return _templateStartingAddress; }
-	} __attribute__((packed));
+	class LogicalMemoryTemplateStartingAddressRegister final {
+        public:
+            static constexpr auto extractTemplateStartingAddress(Ordinal address) noexcept {
+                /**
+                 * Upper 20-bits for the starting address for a logical data
+                 * template. The lower 12 bits are fixed at zero. The starting
+                 * address is modulo 4 kbytes
+                 */
+                return i960::decode<Ordinal, Ordinal, 0xFFFFF000, 0>(address);
+            }
+        public:
+            constexpr LogicalMemoryTemplateStartingAddressRegister() noexcept = default;
+            constexpr LogicalMemoryTemplateStartingAddressRegister(Ordinal raw) noexcept 
+                : _isBigEndian(raw & 1), 
+                _dataCacheEnabled(raw & 2), 
+                _templateStartingAddress(extractTemplateStartingAddress(raw)) { }
+            constexpr auto isLittleEndian() const noexcept { return !_isBigEndian; }
+            constexpr auto isBigEndian() const noexcept { return _isBigEndian; }
+            constexpr bool dataCacheEnabled() const noexcept { return _dataCacheEnabled; }
+            constexpr Ordinal getTemplateStartingAddress() const noexcept { return _templateStartingAddress; }
+            void setTemplateStartingAddress(Ordinal address) noexcept {
+                _templateStartingAddress = extractTemplateStartingAddress(address);
+            }
+            void setDataCacheEnabled(bool value) noexcept { _dataCacheEnabled = value; }
+            void setIsBigEndian(bool value) noexcept { _isBigEndian = value; }
+            constexpr auto getRawValue() const noexcept {
+                return i960::encode<Ordinal, bool, 0x2, 1>(
+                        i960::encode<Ordinal, bool, 0x1, 0>(_templateStartingAddress, _isBigEndian), _dataCacheEnabled);
+            }
+        private:
+            bool _isBigEndian = false; // tied to DLMCON.be and not independent
+            bool _dataCacheEnabled = false;
+            Ordinal _templateStartingAddress = 0;
+	}; 
 	union LogicalMemoryTemplateMaskRegister final {
 		struct {
 			Ordinal _logicalMemoryTemplateEnabled : 1;
